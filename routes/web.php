fa9b328e661026e5ca323502c471f0c0f50818bb
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-24 16:09:40 +0500

    move to create page and back

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('customer.index');
-});
+Route::get('/',[CustomerController::class,'index'])->name('home');
+Route::resource('customers',CustomerController::class);
