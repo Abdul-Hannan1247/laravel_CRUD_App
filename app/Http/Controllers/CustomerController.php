7a3bae7299b5dd516b14a5d7a3a321deb124290a
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-25 23:39:56 +0500

    10_CRUD-Working with show feature

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\CustomerStoreRequest;
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use File;
 
 class CustomerController extends Controller
 {
@@ -59,7 +60,8 @@ class CustomerController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $customer = Customer::findorfail($id);
+        return view('customer.show', compact('customer'));
     }
 
     /**
@@ -67,15 +69,39 @@ class CustomerController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $customer = Customer::findorFail($id);
+        return view('customer.edit',compact('customer'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CustomerStoreRequest $request, string $id)
     {
-        //
+        // dd($request->all());
+        $customer = Customer::findorFail($id);
+
+        if($request->hasFile('image')){
+
+            //Delete previous image
+            File::delete(public_path($customer->image));
+
+            $image =$request->file('image');
+            $fileName = $image->store('','public');
+            $filePath = '/uploads/'.$fileName;
+            $customer->image = $filePath;
+           }
+            $customer->first_name = $request->first_name;
+            $customer->last_name = $request->last_name;
+            $customer->email = $request->email;
+            $customer->phone = $request->phone;
+            $customer->bank_account_number = $request->bank_account_number;
+            $customer->about = $request->about;
+            $customer->save();
+
+            return  redirect()->route('customers.index');
+
+
     }
 
     /**
