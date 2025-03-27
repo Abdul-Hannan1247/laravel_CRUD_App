0897564980cee9dec08c5567491448b36170ab0c
Abdul Hannan <abdulhannan1247@gmail.com>
2025-03-27 18:21:27 +0500

    Calendar added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CalendarController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -30,3 +33,27 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
+
+// **********
+// **********
+// **********
+// **********
+// **********
+// **********
+// ********** Calendar Routes   **************
+
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/test', [UserController::class, 'test']);
+
+// Calendar routes
+Route::get('calendar/index', [CalendarController::class, 'index'])->name('calendar.index');
+Route::post('calendar', [CalendarController::class, 'store'])->name('calendar.store');
+Route::patch('calendar/update/{id}', [CalendarController::class, 'update'])->name('calendar.update');
+Route::delete('calendar/destroy/{id}', [CalendarController::class, 'destroy'])->name('calendar.destroy');
+
+Route::get('books', [BookController::class, 'index'])->name('books.index');
+Route::post('books', [BookController::class, 'store'])->name('books.store');
+
+Route::get('/export-db', function() {
+    return "Exporting";
+});
